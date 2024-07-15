0960a804840815e94f7b28b9de3bf9d963d5cd7e
Elavarasan <contact@elavarasan.me>
2024-07-15 10:28:14 +0530

    backend files updates

--- ReactJs-NodeJs-mySQL/frontend/src/pages/RootPage/components/CardItems.jsx
+++ ReactJs-NodeJs-mySQL/frontend/src/pages/RootPage/components/CardItems.jsx
@@ -6,96 +6,104 @@ import { useEffect, useState } from "react";
 import { FaGithub, FaGithubSquare, FaExternalLinkAlt } from "react-icons/fa";
 import { MdPreview, MdCancel } from "react-icons/md";
 
+const ProjectCard = ({ data }) => {
+  const [showIframe, setShowIframe] = useState(false);
+  console.log(showIframe);
 
-const ProjectCard = ({ data }) => (
-  <div>
-    <dialog
-      id={`modal_${data.id}`}
-      className="prince-modal sm:prince-modal-middle "
-    >
-      <div style={{maxWidth:'80%', maxHeight:'800px'}} className="prince-modal-box prince-bg-transparent ">
-        <div className="prince-border prince-mockup-browser prince-bg-base-300 prince-shadow-black ">
-          <div className="prince-mockup-browser-toolbar">
-            <div className="prince-input">{data.webUrl}</div>
-          </div>
-          <div className=" prince-justify-center prince-flex prism:nce-bg-base-200">
-            <iframe
-              src={data.webUrl}
-              title={data.title}
-              className="prince-w-full  lg:prince-min-h-[500px] prince-min-h-[500px]"
-            ></iframe>
-          </div>
-        </div>
-        <div className="prince-modal-action prince-justify-between">
-          <div className="prince-flex prince-gap-2">
-            <a
-              href={data.github}
-              target="_blank"
-              className="prince-btn prince-rounded-md prince-shadow-md prince-btn-sm "
-            >
-              <FaGithubSquare /> Github
-              <FaExternalLinkAlt />
-            </a>
-            <a
-              href={data.webUrl}
-              target="_blank"
-              className="prince-btn prince-rounded-md prince-btn-primary prince-shadow-md prince-btn-sm"
-            >
-              <MdPreview />
-              Live
-              <FaExternalLinkAlt />
-            </a>
-          </div>
-          <button
-            className="prince-btn prince-rounded-md prince-shadow-md prince-btn-sm prince-btn-error"
-            onClick={() => document.getElementById(`modal_${data.id}`).close()}
-          >
-            <MdCancel />
-            Close
-          </button>
-        </div>
-      </div>
-    </dialog>
-
-    <div className="prince-p-5">
-      <div className="prince-card prince-glass prince-w-72">
-        <figure>
-          {data.previewImg ? (
-            <LazyLoadImage src={data.previewImg} alt={data.title} effect="bur" />
-          ) : (
-            <div className="">
-              <div className="prince-gap-4 prince-flex-col prince-flex w-52">
-                <div className="prince-h-32 prince-w-full prince-skeleton"></div>
-                <div className="prince-h-4 prince-skeleton prince-w-28"></div>
-                <div className="prince-h-4 prince-w-full prince-skeleton"></div>
-                <div className="prince-h-4 prince-w-full prince-skeleton"></div>
-              </div>
+  return (
+    <div>
+      <dialog
+        id={`modal_${data.id}`}
+        className="prince-modal sm:prince-modal-middle"
+        onClose={() => setShowIframe(false)}
+      >
+        <div style={{ maxWidth: '80%', maxHeight: '800px' }} className="prince-modal-box prince-bg-transparent">
+          <div className="prince-border prince-mockup-browser prince-bg-base-300 prince-shadow-black">
+            <div className="prince-mockup-browser-toolbar">
+              <div className="prince-input">{data.webUrl}</div>
             </div>
-          )}
-        </figure>
-        <div className="prince-card-body prince-p-3">
-          <div className="prince-flex prince-justify-between">
-            <a
-              href={data.github}
-              className="prince-btn prince-btn-outline prince-rounded-md prince-btn-sm"
-            >
-              <FaGithub />
-              Github
-            </a>
+            <div className="prince-justify-center prince-flex prince-bg-base-200">
+              {showIframe && (
+                <iframe
+                  src={data.webUrl}
+                  title={data.title}
+                  className="prince-w-full lg:prince-min-h-[500px] prince-min-h-[500px]"
+                ></iframe>
+              )}
+            </div>
+          </div>
+          <div className="prince-modal-action prince-justify-between">
+            <div className="prince-flex prince-gap-2">
+              <a
+                href={data.github}
+                target="_blank"
+                className="prince-btn prince-rounded-md prince-shadow-md prince-btn-sm"
+              >
+                <FaGithubSquare /> Github
+                <FaExternalLinkAlt />
+              </a>
+              <a
+                href={data.webUrl}
+                target="_blank"
+                className="prince-btn prince-rounded-md prince-btn-primary prince-shadow-md prince-btn-sm"
+              >
+                <MdPreview />
+                Live
+                <FaExternalLinkAlt />
+              </a>
+            </div>
             <button
-              onClick={() =>
-                document.getElementById(`modal_${data.id}`).showModal()
-              }
-              className="prince-btn prince-btn-primary prince-rounded-md prince-btn-sm"
+              className="prince-btn prince-rounded-md prince-shadow-md prince-btn-sm prince-btn-error"
+              onClick={() => document.getElementById(`modal_${data.id}`).close()}
             >
-              Preview
+              <MdCancel />
+              Close
             </button>
+          </div>
+        </div>
+      </dialog>
+
+      <div className="prince-p-5">
+        <div className="prince-card prince-glass prince-w-72">
+          <figure>
+            {data.previewImg ? (
+              <LazyLoadImage src={data.previewImg} alt={data.title} effect="blur" />
+            ) : (
+              <div className="">
+                <div className="prince-gap-4 prince-flex-col prince-flex w-52">
+                  <div className="prince-h-32 prince-w-full prince-skeleton"></div>
+                  <div className="prince-h-4 prince-skeleton prince-w-28"></div>
+                  <div className="prince-h-4 prince-w-full prince-skeleton"></div>
+                  <div className="prince-h-4 prince-w-full prince-skeleton"></div>
+                </div>
+              </div>
+            )}
+          </figure>
+          <div className="prince-card-body prince-p-3">
+            <div className="prince-flex prince-justify-between">
+              <a
+                href={data.github}
+                className="prince-btn prince-btn-outline prince-rounded-md prince-btn-sm"
+              >
+                <FaGithub />
+                Github
+              </a>
+              <button
+                onClick={() => {
+                  setShowIframe(true);
+                  document.getElementById(`modal_${data.id}`).showModal();
+                }}
+                className="prince-btn prince-btn-primary prince-rounded-md prince-btn-sm"
+              >
+                Preview
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 const CardItemsTemplate = ({ projects }) => (
   <div className="prince-flex prince-flex-wrap prince-mt-5 prince-justify-center">
